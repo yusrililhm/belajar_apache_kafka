198402b35fb4f6b2d0003917f09d7e36eb40e489
yusrililhm <yusrililham62@gmail.com>
2024-01-01 15:09:41 +0700

    [topic] add: manipulate topic in kafka

---
+++ belajar_apache_kafka.sh
@@ -0,0 +1,10 @@
+
+# Topic
+# create a topic
+./bin/kafka --bootstrap-server host:port --create --topic name
+
+# display all topic
+./bin/kafka --bootstrap-server host:port --list
+
+# delete a topic
+./bin/kafka --bootstrap-server host:port --delete --topic name
