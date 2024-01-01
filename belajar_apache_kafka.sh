894b8867cd44ab753def9c0749261ade11957ddf
yusrililhm <yusrililham62@gmail.com>
2024-01-01 15:57:29 +0700

    [kafka] add: message, producer, and consumer

--- belajar_apache_kafka.sh
+++ belajar_apache_kafka.sh
@@ -1,10 +1,21 @@
 
 # Topic
 # create a topic
-./bin/kafka --bootstrap-server host:port --create --topic name
+./bin/kafka-topics.sh --bootstrap-server host:port --create --topic name
 
 # display all topic
-./bin/kafka --bootstrap-server host:port --list
+./bin/kafka-topics.sh --bootstrap-server host:port --list
 
 # delete a topic
-./bin/kafka --bootstrap-server host:port --delete --topic name
+./bin/kafka-topics.sh --bootstrap-server host:port --delete --topic name
+
+# Message
+# send message to topic
+./bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic name
+
+# Consumer
+# read message from topic
+./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic name --from-beginning
+
+# consumer group
+./bin/kafka-console-consumer.sh --bootstrap-server host:port --topic name --group name --from-beginning
